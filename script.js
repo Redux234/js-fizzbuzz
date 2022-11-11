73fdee9f511a8b203970211c2fdebf561b19988c
Redux234 <lorenzo.gaetano2002@gmail.com>
2022-11-11 16:53:49 +0100

    Finito in console

--- script.js
+++ script.js
@@ -1,13 +1,24 @@
-for(let i = 1; i<=100; i++){
+let Varia
 
-    if((i % 3)==0) {
+for (let i = 1; i <= 100; i++) {
 
-        console.log("Fizz")
+    
+    
+    if(i%3==0&&i%5==0){
+    console.log("FizzBuzz")
+    }else if(i%3==0){
+    console.log("Fizz")
+    }else if(i%5==0){
+    console.log("Buzz")
+    }else   
 
-    }else if((i % 5)==0) {
+console.log(i)
 
-        console.log("Buzz")
-    }else
-        console.log(i)
+
 
 }
+
+
+
+
+
